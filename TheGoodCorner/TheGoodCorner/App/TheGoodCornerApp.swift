e158b61134a614342748e8e3d0015b0c3e7c5c07
Trimech <trimech.sofienne@gmail.com>
2026-05-04 13:30:32 +0200

    chore: setup initial project structure and move resources

---
+++ TheGoodCorner/TheGoodCorner/App/TheGoodCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGoodCornerApp.swift
+//  TheGoodCorner
+//
+//  Created by Sofienne Trimech on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TheGoodCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("TheGoodCorner")
+        }
+    }
+}
